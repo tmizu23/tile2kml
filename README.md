2cb13ab3726601c4756c1148ddf138ef8ffb610a
tmizu23 <mizutani@.(none)>
2012-12-13 15:37:15 +0900

    first commit

--- README.md
+++ README.md
@@ -17,8 +17,16 @@ Dynamic Google Earth SuperOverlay for Ciber Japan Data
  本体です。
 - `cjp.kml`  
  このKMLをGoogleErathで開きます。このKMLからtile2kml.phpを呼び出します。
+- `tile2kml_prezen.php`
+- `prezen.kml`
+ TMSタイルを表示させる例です。
 
-説明
+設置方法
+------
++ tile2kml.phpとcjp.kmlの中の「ユーザー設定」の部分を設置するサーバーのURLに変更してください。
++ tile2kml.phpをサーバーに設置してから、cjp.kmlをGoogle Earthで読み込んでください。
+
+プログラム説明
 -------
 SuperOverlayの仕組みを使ったKMLを作成すれば、表示エリアの地図タイルだけを呼び出しGoogle Eearthに表示させることができます。
 このプログラムは、SuperOverlayのためのKMLを、サーバー側のphpで動的に作成してクライアントに返します。
@@ -32,6 +40,13 @@ OSM,GoogleMapsなどをGoogleEarthに表示させる例です。これにイン�
 SuperOverlayの説明です。
 [https://developers.google.com/kml/articles/phpmysqlkml?hl=ja](https://developers.google.com/kml/articles/phpmysqlkml?hl=ja)
 phpでkmlを出力する方法です。
+
+ご注意
+----------
+ダウンロードしたファイルは、動作確認できるようサーバーURLを設定していますが、
+実際に使用する際は、自前でサーバーをご用意ください。
+動作確認用のサーバーにアクセスが集中し支障が出た場合は、サーバーをストップします。
+
 
 ライセンス
 ----------
--- tile2kml.php
+++ tile2kml.php
@@ -24,12 +24,79 @@ mb_internal_encoding("UTF-8");
 #  DEALINGS IN THE SOFTWARE.
 ********************************************************************************/
 
-//このプログラムを設置するパス
-/////////////////////////////////////////////////////////
+//x,y,zはもとのタイル座標値。TMS=0：XYZ,TMS=1：TMS。cは地図の種類（使用しない場合もある）
+$x = $_GET['x'];
+$y = $_GET['y'];
+$z = $_GET['z'];
+$c = $_GET['c'];
+
+/*************** ユーザー設定  *************************/
+
+//          このプログラムを設置するパス
 
 $phpurl = 'http://www.ecoris.co.jp/map/tile2kml.php';
 
-/////////////////////////////////////////////////////////
+/********************************************************/
+
+///////////////////////////////地図設定/////////////////////////
+
+////////  電子国土用   ////////////
+//(↓は電子国土の場合。レベル19以降は表示しないことにしてある)
+$minx = 121.0;
+$maxx = 154.0;
+$miny = 16.5;
+$maxy = 46.0;
+$minz = 5;
+$maxz = 18;
+$TMS = 0;
+
+$ext = '.png';
+if($c==0){
+ if($z<9){
+  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/JAIS/latest/';
+ }elseif($z<12){
+  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/BAFD1000K/latest/';
+ }elseif($z<15){
+  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/BAFD200K/latest/';
+ }elseif($z<18){
+  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/DJBMM/latest/';
+ }elseif($z<19){
+  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/FGD/latest/';
+ }
+}elseif($c==1){
+ if($z<9){
+  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/RELIEF/latest/';
+ }elseif($z<12){
+  $baseurl = 'http://cyberjapandata2.gsi.go.jp/sqras/all/BAFD1000KG/latest/';
+ }elseif($z<15){
+  $baseurl = 'http://cyberjapandata2.gsi.go.jp/sqras/all/BAFD200KG/latest/';
+ }elseif($z<18){
+  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/DJBMO/latest/';
+  $ext = '.jpg';
+ }elseif($z<19){
+  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/FGD/latest/';
+ }
+}
+
+$xpad = sprintf("%07d", $x);
+$ypad = sprintf("%07d", $y);
+$xystr = substr($xpad,0,1).substr($ypad,0,1).'/'.substr($xpad,1,1).substr($ypad,1,1).'/'.substr($xpad,2,1).substr($ypad,2,1).'/'.substr($xpad,3,1).substr($ypad,3,1).'/'.substr($xpad,4,1).substr($ypad,4,1).'/'.substr($xpad,5,1).substr($ypad,5,1);
+$url = $baseurl . $z . '/' . $xystr . '/' . $xpad . $ypad . $ext;
+/////////  ここまで電子国土用 ////////////
+/*
+//XYZやTMSの場合はこちら
+$minx = 139.238869038526;
+$maxx = 139.463447403723;
+$miny = 37.4356075646937;
+$maxy = 37.613718539038;
+$minz = 5;
+$maxz = 16;
+$TMS = 1;
+$ext = '.png';
+$baseurl = 'http://www.ecoris.co.jp/map/data/prezen/slide/';
+$url = $baseurl . $z . '/' . $x . '/' . $y . $ext;
+*/
+///////////////////////地図設定ここまで//////////////////////////////////////////
 
 $tileSize = 256;
 $originShift = 2 * pi() * 6378137 / 2.0;
@@ -71,61 +138,14 @@ function TileLatLonBounds($tx, $ty, $zoom ){
 }
 
 
-////メイン////////
-//x,y,zはもとのタイル座標値。t=0：XYZ,t=1：TMS。cは地図の種類
-$x = $_GET['x'];
-$y = $_GET['y'];
-$z = $_GET['z'];
-$t = $_GET['t'];
-$c = $_GET['c'];
 //cx,cy,czはTMS座標。内部ではTMS座標で計算
 $cx = $x;
 $cy = $y;
 $cz = $z;
-if($t==0){//XYZの場合TMSに変換
+if($TMS==0){//XYZの場合TMSに変換
  list($cx,$cy) = TMS_XYZ($x,$y,$z);
 }
-
-//XYZやTMSの場合はこちら
-//$ext = '.png';
-//$baseurl = 'http://www.ecoris.co.jp/map/data/geohex/';
-//$url = $baseurl . $z . '/' . $x . '/' . $y . $ext;
-
-//電子国土用
-$ext = '.png';
-if($c==0){
- if($z<9){
-  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/JAIS/latest/';
- }elseif($z<12){
-  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/BAFD1000K/latest/';
- }elseif($z<15){
-  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/BAFD200K/latest/';
- }elseif($z<18){
-  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/DJBMM/latest/';
- }elseif($z<19){
-  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/FGD/latest/';
- }
-}elseif($c==1){
- if($z<9){
-  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/RELIEF/latest/';
- }elseif($z<12){
-  $baseurl = 'http://cyberjapandata2.gsi.go.jp/sqras/all/BAFD1000KG/latest/';
- }elseif($z<15){
-  $baseurl = 'http://cyberjapandata2.gsi.go.jp/sqras/all/BAFD200KG/latest/';
- }elseif($z<18){
-  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/DJBMO/latest/';
-  $ext = '.jpg';
- }elseif($z<19){
-  $baseurl = 'http://cyberjapandata.gsi.go.jp/sqras/all/FGD/latest/';
- }
-}
-
-$xpad = sprintf("%07d", $x);
-$ypad = sprintf("%07d", $y);
-$xystr = substr($xpad,0,1).substr($ypad,0,1).'/'.substr($xpad,1,1).substr($ypad,1,1).'/'.substr($xpad,2,1).substr($ypad,2,1).'/'.substr($xpad,3,1).substr($ypad,3,1).'/'.substr($xpad,4,1).substr($ypad,4,1).'/'.substr($xpad,5,1).substr($ypad,5,1);
-$url = $baseurl . $z . '/' . $xystr . '/' . $xpad . $ypad . $ext;
-////ここまで電子国土用
-
+//タイルの四隅の緯度経度を計算
 $bounds = TileLatLonBounds($cx, $cy, $cz );
 
 //Create KML
@@ -155,8 +175,8 @@ $kml[] = ' </LatLonAltBox>';
 $kml[] = ' </Region>';
 
 //This Zoom Image
-//対象範囲以外は画像を表示させない。(↓は電子国土の場合。レベル19以降も表示しないことにしてある)
-if((5<$z && $z <18 && 16<=$bounds[0] && $bounds[2]<=48 && 121 <= $bounds[1] && $bounds[3] <= 158)|| ($z==5 && 26<=$x && $x<=29 && 11<=$y && $y<=14)){
+//対象範囲以外は画像を表示させない。
+if(($minz<=$z && $z <=$maxz && $miny<$bounds[2] && $bounds[0]<$maxy && $minx < $bounds[3] && $bounds[1] < $maxx)){
  $kml[] = ' <GroundOverlay>';
  $kml[] = ' <drawOrder>'. $z .'</drawOrder>';
  $kml[] = ' <Icon>';
@@ -171,7 +191,7 @@ if((5<$z && $z <18 && 16<=$bounds[0] && $bounds[2]<=48 && 121 <= $bounds[1] && $
  $kml[] = ' </GroundOverlay>';
 }
 
-if($z<18){
+if($z<$maxz){
  //Next Zoom Link
  for($i=0;$i<=1;$i++){
 	for($j=0;$j<=1;$j++){
@@ -179,7 +199,7 @@ if($z<18){
 		$ny = 2*$cy + $j;
 		$nz = $cz + 1;
 		$bounds = TileLatLonBounds($nx, $ny, $nz );
-		if($t==0){
+		if($TMS==0){
 			list($nx,$ny) = TMS_XYZ($nx,$ny,$nz);
 		}
 		$kml[] = ' <NetworkLink>';
@@ -198,7 +218,7 @@ if($z<18){
 		$kml[] = ' </Region>';
 		$kml[] = ' <Link>';
 		$kml[] = ' <href>' . $phpurl . '</href>';
-		$kml[] = ' <httpQuery>x='. $nx . '&amp;y=' . $ny . '&amp;z=' . $nz . '&amp;t=' . $t . '&amp;c=' . $c .'</httpQuery>';
+		$kml[] = ' <httpQuery>x='. $nx . '&amp;y=' . $ny . '&amp;z=' . $nz . '&amp;c=' . $c .'</httpQuery>';
 		$kml[] = ' <viewRefreshMode>onRegion</viewRefreshMode>';
 		$kml[] = ' <viewFormat/>';
 		$kml[] = ' </Link>';
